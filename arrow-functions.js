310915c0e13456fbb612f1396cbe8c5d32e3eb16
nirjalatripathi <nirjalatripathi41@gmail.com>
2026-01-13 21:13:52 +0545

    [ADD]: celsius to fahrenheit

--- arrow-functions.js
+++ arrow-functions.js
@@ -10,3 +10,8 @@ console.log ("The area of rectangle is", calculateArea (5,8));
 // Q.3. Odd or even 
 const isEven = (num) => num % 2 ===0;
 console.log ("Is 13 even? ", isEven(13));
+
+// Q.4. Convert Celsius to Fahrenheit
+const celsiusToFahrenheit = (celsius) => (celsius * 9/5) +32;
+console.log ("5 degree C in F is", celsiusToFahrenheit(5));
+
