017c219bf88d4175c2e931449b7ccdbc77feb173
nirjalatripathi <nirjalatripathi41@gmail.com>
2026-01-14 20:46:29 +0545

    [ADD]: Array of numbers

---
+++ arrays.js
@@ -0,0 +1,7 @@
+// TOPIC 2: ARRAYS 
+//Q.1. Create an array of numbers and print all the elements using a loop
+let numbers =[1, 2, 3, 4, 5];
+for (let i=0; i< numbers.length; i++) {
+    console.log (numbers[i]);
+
+}
