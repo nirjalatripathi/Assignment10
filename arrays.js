fd96e24ccfe20ca83df0e7c79e320abf307b142e
nirjalatripathi <nirjalatripathi41@gmail.com>
2026-01-14 20:52:22 +0545

    [ADD]: sum of all elements

--- arrays.js
+++ arrays.js
@@ -3,5 +3,12 @@
 let numbers =[1, 2, 3, 4, 5];
 for (let i=0; i< numbers.length; i++) {
     console.log (numbers[i]);
-
 }
+
+//Q.2. Given an array of numbers, find the sum of all elements
+let number= [5, 10, 15, 20, 25];
+let sum=0;
+for (let i=0; i < number.length; i++) {
+    sum= sum +numbers[i];
+}
+console.log ("The sum of all elements is", sum);
