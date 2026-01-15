080bc9275f2df22b4889f3314771131fa2bbcd79
nirjalatripathi <nirjalatripathi41@gmail.com>
2026-01-15 12:13:06 +0545

    [ADD]: Object showing all properties

---
+++ objects.js
@@ -0,0 +1,8 @@
+//TOPIC 3: OBJECTS
+//Q.1. Create an object that stores a person’s name, age, and city, then print all the Properties.
+let person={
+    name:" Nirjala",
+    age: 19,
+    city:"Kathmandu"
+}
+console.log(person);
